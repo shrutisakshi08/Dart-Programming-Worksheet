514826f58692cac7ae7c669b3fddc970b06da496
Prashant K Pathak <123973637+prashant152005@users.noreply.github.com>
2024-12-24 01:13:29 +0530

    Update arith_operations.dart

--- arith_operations.dart
+++ arith_operations.dart
@@ -1,16 +1,30 @@
+import 'dart:io';
+
 void main() {
-  int a = 10;
-  int b = 5;
-//Sum 
-int sum = a+b;
-print("Sum: $sum");
-// Difference 
-int difference = a-b;
-print("Difference: $difference");
-// Product 
-int product = a*b;
-print("Product: $product");
-// Quotient
-double quotient = a/b;
-print("Quotient: $quotient");
+  // User input for the values of a and b
+  print("Enter the first number (a): ");
+  int a = int.parse(stdin.readLineSync()!);
+
+  print("Enter the second number (b): ");
+  int b = int.parse(stdin.readLineSync()!);
+
+  // Sum
+  int sum = a + b;
+  print("Sum: $sum");
+
+  // Difference
+  int difference = a - b;
+  print("Difference: $difference");
+
+  // Product
+  int product = a * b;
+  print("Product: $product");
+
+  // Quotient
+  if (b != 0) {
+    double quotient = a / b;
+    print("Quotient: $quotient");
+  } else {
+    print("Quotient: Division by zero is not allowed");
+  }
 }
